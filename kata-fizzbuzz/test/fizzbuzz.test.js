c3949b740cce7c2376d93d75dd67fb83815db7f1
apecr <albertoeyo@outlook.es>
2017-02-20 21:12:00 +0100

    Tema 3.6
    Tema 3.6 Test doubles parte iv (mocks)

--- kata-fizzbuzz/test/fizzbuzz.test.js
+++ kata-fizzbuzz/test/fizzbuzz.test.js
@@ -5,47 +5,52 @@ var FizzBuzz = require('../fizzbuzz.js');
 
 suite('Print', function(){
   var fizzBuzz = null;
-  var database = null;
+  var mockDatabase = null;
   setup( function(){
-    this.database = {
+    var database = {
       initConnection : function(){},
       getStringWhenThreeNumber : function(){}
     }
-    this.fizzBuzz = new FizzBuzz(this.database);
+    this.mockDatabase = sinon.mock(database);
+    this.fizzBuzz = new FizzBuzz(database);
   });
   test('return 1 when print number one', function(){
     //Arrange
+    this.mockDatabase.expects('initConnection').never();
     //Act
     var result = this.fizzBuzz.print(1);
     //assert
-    assert.ok(this.database.initConnection.called);
+    this.mockDatabase.verify();
     assert.equal('1', result);
   });
   test('return 2 when print number two', function(){
     //Arrange
+    this.mockDatabase.expects('initConnection').never();
     //Act
     var result = this.fizzBuzz.print(2);
     //assert
-    assert.ok(this.database.initConnection.called);
+    this.mockDatabase.verify();
     assert.equal('2', result);
   });
   test('Throw exception when argument is string', function(){
     //Arrange
+    this.mockDatabase.expects('initConnection').never();
     //Act
     //assert
     var self = this;
     assert.throws(function(){
       self.fizzBuzz.print('two')
     }, /Number is not an Integer/);
+    this.mockDatabase.verify();
   });
   test('return Fizz when print number 3. We are calling to the database', function(){
     //Arrange
-    sinon.spy(this.database, 'initConnection');
-    sinon.stub(this.database, 'getStringWhenThreeNumber').returns('Fizz');
+    this.mockDatabase.expects('initConnection').once();
+    this.mockDatabase.expects('getStringWhenThreeNumber').once().returns('Fizz');
     //Act
     var result = this.fizzBuzz.print(3);
     //assert
-    assert.ok(this.database.initConnection.called);
+    this.mockDatabase.verify();
     assert.equal('Fizz', result);
   });
 });
