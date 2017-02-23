c9df6e5343798d5df28242fbe7960eb271941684
alberto <alberto@alberto-HP-EliteBook-840-G2>
2017-02-23 16:09:13 +0100

    Inicio del tema 4
    Inicio del tema 4

---
+++ list-manager/test/list-test.js
@@ -0,0 +1,16 @@
+/**
+ * http://usejsdoc.org/
+ */
+var assert = require('assert');
+var sinon = require('sinon');
+var jsonfile = require('jsonfile');
+var List = require('../list.js');
+
+suite('List', function(){
+	setup(function(){});
+	test('should get all lists', function(){
+		//Arrange
+		//Act
+		//Assert
+	});
+});
