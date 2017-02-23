9f48ff02d482dbe1bfd05a2760204b39a53aeea3
alberto <alberto@alberto-HP-EliteBook-840-G2>
2017-02-23 17:00:17 +0100

    Primer video tema 4
    Primer video del tema 4

--- list-manager/list.js
+++ list-manager/list.js
@@ -1,3 +1,9 @@
 /**
  * http://usejsdoc.org/
  */
+module.exports = function List(){
+  const JSON_FILENAME = 'lists.json';
+  this.getLists = function(jsonfile){
+    return jsonfile.readFileSync(JSON_FILENAME);
+  }
+};
--- list-manager/test/list-test.js
+++ list-manager/test/list-test.js
@@ -10,7 +10,22 @@ suite('List', function(){
 	setup(function(){});
 	test('should get all lists', function(){
 		//Arrange
+		var mockJsonfile = sinon.mock(jsonfile);
+		var filename = 'lists.json';
+		var listSalida = {};
+		mockJsonfile.expects('readFileSync').once().withArgs(filename).returns(listSalida);
+		var listObject = new List();
 		//Act
+		var result = listObject.getLists(jsonfile);
+		//Assert
+		assert.equal(listSalida, result);
+		mockJsonfile.verify();
+	});
+	test('should create a new list with name', function(){
+		//Arrange
+		var mockJsonfile = sinon.mock(jsonfile);
+		//Act
+		listObject.createList(listName);
 		//Assert
 	});
 });
