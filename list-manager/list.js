4b6a84e2c9830fbc029e628f111e6a23f5fae968
alberto <albertoeyo@outlook.es>
2017-03-01 16:51:25 +0100

    Tema 4-4
    Fin tema 4-4

--- list-manager/list.js
+++ list-manager/list.js
@@ -7,18 +7,48 @@ module.exports = function List(jsonfile){
   this.getLists = function(){
     return jsonfile.readFileSync(JSON_FILENAME);
   };
+
   this.createList = function(listName){
     var listCollection = this.getLists();
     this.throwErrorIfListExistWhenCreate(listCollection, listName);
 		listCollection[listName] = [];
-    jsonfile.writeFileSync(JSON_FILENAME, listCollection);
+    this.writeListCollectionInJson(listCollection);
+  };
+
+  this.removeList = function(listName){
+    var listCollection = this.getLists();
+    this.forInListCollectionAndListEqualListNameExecuteFunction(
+      listCollection,
+      listName,
+      'deleteListInCollectionAndWriteListCollectionJson',
+      [listName, listCollection]);
   };
 
   this.throwErrorIfListExistWhenCreate = function(listCollection, listName){
+    this.forInListCollectionAndListEqualListNameExecuteFunction(
+      listCollection,
+      listName,
+      'throwErrorListExist',
+      []);
+  };
+
+  this.forInListCollectionAndListEqualListNameExecuteFunction = function(listCollection, listName, functionName, params){
     for (var list in listCollection){
       if (list === listName){
-          throw new Error('List exists!');
+          this[functionName].apply(this, params);
       }
     }
   }
+
+  this.deleteListInCollectionAndWriteListCollectionJson = function(listName, listCollection){
+    delete listCollection[listName];
+    this.writeListCollectionInJson(listCollection);
+  }
+
+  this.throwErrorListExist = function(){
+    throw new Error('List exists!');
+  }
+  this.writeListCollectionInJson =  function(listCollection){
+    jsonfile.writeFileSync(JSON_FILENAME, listCollection);
+  };
 };
